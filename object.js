afd12812d57d07725ed7bf596a6bfec7e7d01ae3
cpatelis <patelischristos@gmail.com>
2026-05-10 12:36:58 +0300

    object's creation

---
+++ object.js
@@ -0,0 +1,15 @@
+const user = {
+    name: "Christos",
+    age: 55,
+    idStudent: true,
+};
+
+const car ={
+    name: "Renault",
+    type: "Trafic",
+    power: 155,
+    year: 2007,
+};
+
+console.log (user);
+console.log(car);
