403649e4bcb0d674528676292de4b0b095e15082
cpatelis <patelischristos@gmail.com>
2026-05-10 12:22:56 +0300

    Feature: check even or odd

--- funcAdd.js
+++ funcAdd.js
@@ -1,6 +1,17 @@
 function add(a,b){
-    console.log("A+B=",a+b);
+   console.log("A+B=",a+b);
+    return a+b
+    
 }
 
-add(5,3);
+const check = (num)=>{
+    if (num %2 ===0){
+        console.log(num,"Is even.");
+    }else{
+        console.log(num,"Is odd.");
+    }
+}
+check(add(5,3));
+check(add(5,4));
 add(125,763);
+
