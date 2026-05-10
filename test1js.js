5a831be1525ced12af70fbe27656b9742b3297be
cpatelis <patelischristos@gmail.com>
2026-05-10 11:39:53 +0300

    Absences, grade scale

--- test1js.js
+++ test1js.js
@@ -1,14 +1,23 @@
-let mathGrade =18;
-let historyGrade = 19;
-let readingGrade =16;
+let mathGrade =13;
+let historyGrade = 7;
+let readingGrade =5;
+let absences=15;
 
 let average=(mathGrade+historyGrade+readingGrade)/3;
 
 console.log("Your average is:");
 console.log(average);
 
-if (average>=10){
-    console.log("Congratulations you passed!!!")
+if(absences<=50){
+    if (average>=18){
+        console.log("Excelent!!!");
+    }else if (average>=14){
+        console.log("Good!");
+    }else if (average>=10){
+        console.log("Base.");
+    }else{
+        console.log("You Failed...");
+    }
 }else{
-    console.log("Sory....You failed.")
+    console.log("Failed, too many absences!!!!" )
 }
