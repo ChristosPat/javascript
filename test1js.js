8f16e00e638e28951c47e1b6a257217d009c97c8
cpatelis <patelischristos@gmail.com>
2026-05-10 11:16:18 +0300

    first excersise

---
+++ test1js.js
@@ -0,0 +1,8 @@
+let mathgrade =18;
+let historyGrade = 19;
+let readingGrade =16;
+
+let average=(mathgrade+historyGrade+readingGrade)/3;
+
+console.log("ο μέσος όρος είναι:");
+console.log(average);
